dc7a4487d7880caef8ce14f38460dc15f58bec5d
Daeaznar <dae.aznar@gmail.com>
2021-01-26 19:40:03 -0600

    Finished Fibonacci & Factorial

---
+++ CursoPVE/CharacterCount/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CharacterCount
+{
+    public partial class charCount : Form
+    {
+        public charCount()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            int count_v = 0, count_c =0, count_n = 0, count_x=0;
+            char[] vowels = "aeiouAEIOU".ToCharArray();
+            char[] cons = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".ToCharArray();
+            char[] numbers = "0123456789".ToCharArray();
+
+            string text_in = txtText.Text;
+
+            for (int i = 0; i < text_in.Length; i++)
+            {
+                if (vowels.Contains(text_in[i]))
+                {
+                    ++count_v;
+                }
+                else
+                {
+                    if (cons.Contains(text_in[i]))
+                    {
+                        ++count_c;
+                    }
+                    else
+                    {
+                        if (numbers.Contains(text_in[i]))
+                        {
+                            ++count_n;
+                        }
+                        else
+                        {
+                            ++count_x;
+                        }
+                    }
+                }
+            }
+
+            lblVowels.Text += $" {count_v}";
+            lblConsonants.Text += $" {count_c}";
+            lblNumbers.Text += $" {count_n}";
+            if (count_x >0)
+            {
+                lblOthers.Text = $"Others: {count_x}";
+            }
+            
+        }
+    }
+}
---
+++ CursoPVE/FibonacciFactorial/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FibonacciFactorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number = 0;
+            bool result = int.TryParse(txtNumber.Text, out number);
+            if (result == true)
+            { 
+                if (number>=1)
+                {
+                    //Fibonacci
+                    long[] fib = new long[number + 1];
+                    fib[0] = 0;
+                    fib[1] = 1;
+                    for (int i = 2; i < number+1; i++)
+                    {
+                        fib[i] = fib[i - 1] + fib[i - 2];
+                    }
+                    lblFibonacci.Text = "Fibonacci: ";
+                    foreach (int i in fib)
+                    {
+                        lblFibonacci.Text += $"{i}, ";
+                    }
+
+                    //Factorial
+                    int fact = 1;
+                    for (int i = number; i > 0; i--)
+                    {
+                        fact *= i;
+                    }
+                    lblFactorial.Text = $"Factorial: {fact}";
+                }
+                else
+                {
+                    MessageBox.Show("Ingrese un número mayor a 0");
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("El dato ingresado NO es válido");
+            }
+        }
+    }
+}
