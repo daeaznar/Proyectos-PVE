6309beafa89208c4af184a583617b970cfe57b0b
Daeaznar <dae.aznar@gmail.com>
2021-01-27 21:21:34 -0600

    Created Student List app

---
+++ CursoPVE/ControlsAndWindows/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace ControlsAndWindows
+{
+    partial class ControlsAndWindows
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lstStudents = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblGrade = new System.Windows.Forms.Label();
+            this.cmbGrades = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(58, 37);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(49, 20);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtName.Location = new System.Drawing.Point(62, 62);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(392, 27);
+            this.txtName.TabIndex = 1;
+            // 
+            // lstStudents
+            // 
+            this.lstStudents.FormattingEnabled = true;
+            this.lstStudents.ItemHeight = 20;
+            this.lstStudents.Location = new System.Drawing.Point(62, 110);
+            this.lstStudents.Name = "lstStudents";
+            this.lstStudents.Size = new System.Drawing.Size(392, 224);
+            this.lstStudents.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(649, 61);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 28);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblGrade
+            // 
+            this.lblGrade.AutoSize = true;
+            this.lblGrade.Location = new System.Drawing.Point(479, 37);
+            this.lblGrade.Name = "lblGrade";
+            this.lblGrade.Size = new System.Drawing.Size(49, 20);
+            this.lblGrade.TabIndex = 4;
+            this.lblGrade.Text = "Grade";
+            // 
+            // cmbGrades
+            // 
+            this.cmbGrades.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGrades.FormattingEnabled = true;
+            this.cmbGrades.Items.AddRange(new object[] {
+            "First",
+            "Second",
+            "Third"});
+            this.cmbGrades.Location = new System.Drawing.Point(483, 62);
+            this.cmbGrades.Name = "cmbGrades";
+            this.cmbGrades.Size = new System.Drawing.Size(121, 28);
+            this.cmbGrades.TabIndex = 5;
+            // 
+            // ControlsAndWindows
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(764, 386);
+            this.Controls.Add(this.cmbGrades);
+            this.Controls.Add(this.lblGrade);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstStudents);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ControlsAndWindows";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Controls & Windows";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.ListBox lstStudents;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblGrade;
+        private System.Windows.Forms.ComboBox cmbGrades;
+    }
+}
+
---
+++ CursoPVE/ControlsAndWindows/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlsAndWindows
+{
+    public partial class ControlsAndWindows : Form
+    {
+        public ControlsAndWindows()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //Validates if the txtBox is empty or not
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                // Error MessageBox with message and caption
+                MessageBox.Show("This field Name CAN'T be empty", "Error");
+                return;
+            }
+            if (lstStudents.Items.Contains(txtName.Text.ToUpper()))
+            {
+                MessageBox.Show("This student is already registered", "Error");
+                return;
+            }
+            // Counts number of items        var count = lstStudents.Items.Count;
+            // Clears the list      lstStudents.Items.Clear();
+            //Adds txtName (in uppercase) to list 
+            if (cmbGrades.SelectedItem ==null)
+            {
+                MessageBox.Show("Please select a Grade","Error");
+                return;
+            }
+            lstStudents.Items.Add($"{txtName.Text.ToUpper()} {cmbGrades.SelectedItem}");
+        }
+    }
+}
