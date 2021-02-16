c4a177a084ef842cd1510ad3f0717dc83e21b6fa
David Hernandez <dae.aznar@gmail.com>
2021-02-16 15:00:37 -0600

    Finished GUI and timer

---
+++ CursoPVE/MemoryGame_MatchingPairs/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryGame_MatchingPairs
+{
+    public partial class Form1 : Form
+    {
+        private int tickCountSec;
+        private int tickCountMin;
+        public Form1()
+        {
+            tickCountSec = 0;
+            tickCountMin = 0;
+            InitializeComponent();
+        }
+
+        private void tmrSeconds_Tick(object sender, EventArgs e)
+        {
+            ++tickCountSec;
+            if (tickCountSec == 60)
+            {
+                tickCountSec = 0;
+                ++tickCountMin;
+            }
+
+            if (tickCountMin < 10)
+            {
+                if (tickCountSec < 10)
+                {
+                    lblTimer.Text = $"Timer: 0{tickCountMin}:0{tickCountSec}";
+                }
+                else
+                {
+                    lblTimer.Text = $"Timer: 0{tickCountMin}:{tickCountSec}";
+                }
+
+            }
+            else
+            {
+                if (tickCountSec < 10)
+                {
+                    lblTimer.Text = $"Timer: {tickCountMin}:0{tickCountSec}";
+                }
+                else
+                {
+                    lblTimer.Text = $"Timer: {tickCountMin}:{tickCountSec}";
+                }
+
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            tmrSeconds.Start();
+        }
+    }
+}
